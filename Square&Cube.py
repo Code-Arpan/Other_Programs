0fe53557a37bd7d9e74163dbec3a8654bd7ef092
Arpan <arpan.pal1966@gmail.com>
2022-02-22 18:15:15 +0530

    Python Calculation

---
+++ Square&Cube.py
@@ -0,0 +1,7 @@
+i = input("Enter Number: ") 
+
+s = int(i)**2    #calculating Square
+c = int(i)**3    #calculating Cube
+
+print("Square of "+str(i)+" is "+str(s))
+print("Cube of "+str(i)+" is "+str(c))
