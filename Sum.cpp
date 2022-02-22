ef9d67292dbd4ee2b8f688449f4b54a164fa4036
Arpan <arpan.pal1966@gmail.com>
2022-02-22 10:49:00 +0530

    Initial Commit

---
+++ Sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num_1, num_2, sum;
+    
+    cout<<"Enter 1st Number\n";
+    cin>>num_1;
+    
+    cout<<"Enter 2nd Number\n";
+    cin>>num_2;
+
+    sum=num_1+num_2;
+    cout<<"Sum = "<<sum;
+    return 0;
+}
